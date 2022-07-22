f7fb5ad2827102f966aee9118e348bf796101cfb
KutsevEA <kucev.ea@gmail.com>
2022-07-22 12:28:03 +0300

    1-9 Done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const submitForm = document.querySelector(`.login-form`);
 
+submitForm.addEventListener(`submit`, handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+  if (this.email.value === `` || this.password.value === ``) {
+      return console.log(`Please fill in all the fields!`);
+  }
+    const formValue = { email: this.email.value, password: this.password.value };
+    console.log(formValue);
+    event.currentTarget.reset();
+
+    
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(`.change-color`);
+const colorValueOnScreen = document.querySelector(`.color`);
+const body = document.querySelector(`body`);
+
+changeColorBtn.addEventListener(`click`, () => {
+
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorValueOnScreen.textContent = `${getRandomHexColor()}`;
+}
+)
