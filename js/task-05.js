b1e68e8a1b9780b76451867655ca48479359ea63
KutsevEA <kucev.ea@gmail.com>
2022-07-24 13:32:45 +0300

    fix

--- js/task-05.js
+++ js/task-05.js
@@ -3,11 +3,7 @@ const output = document.querySelector('#name-output');
 
 textInput.addEventListener('input', event => {
   output.textContent = event.currentTarget.value;
-});
-
-document.addEventListener('keyup', () => {
   if (output.textContent === ``) {
     output.textContent = `Anonymous`;
   }
 });
-
--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,15 @@
 const inputEl = document.querySelector(`#validation-input`);
 
+const lengthValue = Number(inputEl.getAttribute('data-length'));
+
 function validation() {
-  if (inputEl.value.length < inputEl.getAttribute('data-length')) {
-    inputEl.classList.add(`invalid`);
-  } else inputEl.classList.add(`valid`);
+  if (inputEl.value.length === lengthValue) {
+    inputEl.classList.add(`valid`);
+  } else inputEl.classList.add(`invalid`);
 }
 
 inputEl.addEventListener(`blur`, validation);
-inputEl.addEventListener(`focus`, () => {inputEl.classList.remove(`invalid`); inputEl.classList.remove(`valid`); });
+inputEl.addEventListener(`focus`, () => {
+  inputEl.classList.remove(`invalid`);
+  inputEl.classList.remove(`valid`);
+});
